ea318db64d84514dc9d39716767c29f8d7a64e91
Michael <m.rade@jerio.de>
2016-06-29 18:56:20 +0200

    hisat2

---
+++ include/steps/hisat2.py
@@ -0,0 +1,161 @@
+import sys
+import yaml
+import os
+from logging import getLogger
+from abstract_step import AbstractStep
+
+logger=getLogger('uap_logger')
+
+class Hisat2(AbstractStep):
+    '''
+
+    '''
+    
+    def __init__(self, pipeline):
+        super(Hisat2, self).__init__(pipeline)
+        self.set_cores(6)
+
+        self.add_connection('in/first_read')
+        self.add_connection('in/second_read')
+        self.add_connection('out/alignments')
+        self.add_connection('out/log_stderr')
+        self.add_connection('out/metrics')
+
+
+        self.require_tool('dd')
+        self.require_tool('mkfifo')
+        self.require_tool('pigz')
+        self.require_tool('hisat2')
+
+        self.add_option('index', str, optional=False,
+                        description="Path to hisat2 index (not containing file "
+                        "suffixes).")
+                        
+        self.add_option('cores', int, default=12)
+
+    def runs(self, run_ids_connections_files):
+        self.set_cores(self.get_option('cores'))
+
+        # Check if option values are valid
+        if not os.path.exists(self.get_option('index') + '.1.ht2'):
+            logger.error("Could not find index file: %s.*" %
+                         self.get_option('index'))
+            sys.exit(1)
+
+        for run_id in run_ids_connections_files.keys():
+            with self.declare_run(run_id) as run:
+                # Get list of files for first/second read
+                fr_input = run_ids_connections_files[run_id]['in/first_read']
+                sr_input = run_ids_connections_files[run_id]['in/second_read']
+
+                
+                input_paths = [ y for x in [fr_input, sr_input] \
+                               for y in x if y !=None ]                    
+
+                # Do we have paired end data and is it exactly one ?
+                is_paired_end = True
+                if sr_input == [None]:
+                    is_paired_end = False
+
+                with run.new_exec_group() as exec_group:
+                    # Lists of fifos
+                    fr_temp_fifos = list()
+                    sr_temp_fifos = list()
+   
+                    def prepare_input(input_path, exec_group, temp_fifos):
+                        # Create temporary fifo
+                        temp_fifo = run.add_temporary_file(
+                            'in-fifo-%s' %
+                            os.path.basename(input_path), suffix='.fastq' )
+                        mkfifo = [self.get_tool('mkfifo'), temp_fifo]
+                        exec_group.add_command(mkfifo)
+                        temp_fifos.append(temp_fifo)
+                        # Is input gzipped fasta?
+                        is_fastq_gz = False
+                        
+
+                        
+                        for suff  in ['fq.gz', 'fastq.gz','.gz']:
+                            if input_path.endswith(suff):
+                                is_fastq_gz = True
+
+                        # If yes we need to decompress it
+                        if is_fastq_gz:
+                            with exec_group.add_pipeline() as unzip_pipe:
+                                # 2.1 command: Read file in 4MB chunks
+                                dd_in = [self.get_tool('dd'),
+                                      'bs=4M',
+                                      'if=%s' % input_path]
+                                unzip_pipe.add_command(dd_in)
+                                # 2.2 command: Uncompress data
+                                pigz = [self.get_tool('pigz'),
+                                        '--decompress',
+                                        '--stdout']
+                                unzip_pipe.add_command(pigz)
+                                # 2.3 Write file in 4MB chunks to fifo
+                                dd_out = [self.get_tool('dd'),
+                                          'obs=4M',
+                                          'of=%s' % temp_fifo]
+                                unzip_pipe.add_command(dd_out)
+                        else:
+                            dd = [self.get_tool('dd'),
+                                  'bs=4M',
+                                  'if=%s' % input_path,
+                                  'of=%s' % temp_fifo]
+                            exec_group.add_command(dd)
+
+                        return (exec_group, temp_fifos)
+
+                    for input_path in fr_input:
+                        exec_group, fr_temp_fifos = prepare_input(
+                            input_path, exec_group, fr_temp_fifos)
+                    # And if we handle paired end data 
+                    if is_paired_end:
+                        for input_path in sr_input:
+                            exec_group, sr_temp_fifos = prepare_input(
+                                input_path, exec_group, sr_temp_fifos)
+
+                    # 3. Map reads using hisat2
+                    with exec_group.add_pipeline() as hisat2_pipe:
+                        # Assemble hisat2 command
+                        hisat2 = [
+                            self.get_tool('hisat2'), '-q',
+                            '-p', str(self.get_option('cores')),
+                            '-x', self.get_option('index')
+                        ]
+                        if is_paired_end:
+                            hisat2.extend([
+                                '-1', ','.join(fr_temp_fifos),
+                                '-2', ','.join(sr_temp_fifos)])
+                        else:
+                            hisat2.extend(['-U', ','.join(fr_temp_fifos)])
+
+                        
+                        log_stderr = run.add_output_file(
+                                'log_stderr',
+                                '%s-hisat2-log_stderr.txt' % run_id,
+                                input_paths)
+
+                        metrics = run.add_output_file(
+                                'metrics',
+                                '%s-hisat2-metrics.txt' % run_id,
+                                input_paths)
+                        hisat2.extend(['--met-file', metrics])
+                        hisat2_pipe.add_command(hisat2, stderr_path=log_stderr)
+                        
+                        # Compress hisat2 output
+                        pigz = [self.get_tool('pigz'),
+                                '--stdout']
+                        hisat2_pipe.add_command(pigz)
+                        # Write hisat2 output to file
+                        dd = [
+                            self.get_tool('dd'),
+                            'obs=4M',
+                            'of=%s' %
+                            run.add_output_file(
+                                'alignments',
+                                '%s-hisat2-results.sam.gz' % run_id,
+                                input_paths
+                            )
+                        ]
+                        hisat2_pipe.add_command(dd)
